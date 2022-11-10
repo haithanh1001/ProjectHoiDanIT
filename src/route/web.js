a0ba17406ff12c232d0bc723bc32c7ee0e931b1b
haithanh <haithanh9718@gmail.com>
2022-11-10 17:16:25 +0700

    Thiet ke project theo dung mo hinh MVC

---
+++ src/route/web.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import homeController from '../controller/homeController'
+let router = express.Router();
+const initWebRoute = (app) =>{
+    router.get('/',homeController.getHomePage);
+    router.get('/about',(req,res)=>{
+        res.send('Toi la bi nguyen');
+    })
+    return app.use('/',router);
+}
+
+module.exports = initWebRoute;
--- src/server.js
+++ src/server.js
@@ -2,16 +2,15 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
 require('dotenv').config();
+import initWebRoute from './route/web';
 const app = express();
 const port = process.env.PORT || 3001;
+// setup view engine
 configViewEngine(app);
-app.get('/',(req,res)=>{
-    res.render('index');
-})
 
-app.get('/about',(req,res)=>{
-    res.send('Toi la bi nguyen');
-})
+//init web route
+initWebRoute(app);
+
 app.listen(port,()=>{
     console.log(`Example app listening on port: ${port}`);
 })
