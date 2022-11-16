1bb551ffb691004b83e1a83c1a4cd707f29f5c26
haithanh <haithanh9718@gmail.com>
2022-11-17 00:50:27 +0700

    RESTful API

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import express from 'express';
 import configViewEngine from './configs/viewEngine';
 require('dotenv').config();
 import initWebRoute from './route/web';
+import initAPIRoute from './route/api';
 import connection from './configs/connectDB.js';
 // const connection = require('./configs/connectDB');
 const app = express();
@@ -15,7 +16,7 @@ configViewEngine(app);
 //init web route
 initWebRoute(app);
 
-
+initAPIRoute(app);
 
 app.listen(port,()=>{
     console.log(`Example app listening on port: ${port}`);
